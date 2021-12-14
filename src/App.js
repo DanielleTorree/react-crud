191df5daade50c0398ef553c46dd40a8f79d91c0
Danielle Torres de Freitas <danielletorree01@gmail.com>
2021-12-14 17:48:22 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
-function App() {
+// components
+import Menu from './components/Menu';
+import TabelaLivros from './components/TabelaLivros';
+import CadastrarLivros from './components/CadastrarLivros';
+import NotFound from './components/NotFound';
+
+const App = () => {
+
+  const [livros, setLivros] = useState([]);
+
+  const inserirLivro = (livro) => {
+    livro.id = livro.isbn;
+    setLivros(prevLivros => ([...prevLivros, livro]));
+  }
+
+  const editarLivro = (livro) => {
+    const index = livros.findIndex(p => p.id === livro.id);
+    const listaLivros = livros
+      .slice(0, index)
+      .concat(livros.slice(index + 1));
+    
+    const newLivros = [...listaLivros, livro].sort((a, b) => a.id - b.id);
+    setLivros(newLivros);
+  }
+
+  const removerLivro = (idLivro) => {
+    if(window.confirm('Remover este livro?')){
+      const novaListaLivros = livros.filter(livro => livro.id !== idLivro);
+      setLivros(novaListaLivros)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Menu />
+        <Switch>
+          <Route 
+            exact path='/' 
+            render={() => 
+              <TabelaLivros 
+                livros={livros} 
+                removerLivro={removerLivro}
+              />
+            }
+          />
+          <Route 
+            exact 
+            path='/cadastrar' 
+            render={() => 
+              <CadastrarLivros 
+                inserirLivro={inserirLivro}
+                livro={{id: 0, isbn: '', titulo:'', autor:''}}
+              />
+            }
+          />
+          <Route 
+            exact
+            path='/editar/:isbnLivro'
+            render={(props) => {
+              const livro = livros.find(
+                livro => livro.isbn === props.match.params.isbnLivro
+              )
+              if(livro){
+                return (
+                  <CadastrarLivros 
+                    editarLivro={editarLivro}
+                    livro={livro}
+                  />
+                );
+              } else{
+                return <Redirect to='/' />  
+              }   
+            }}
+          />
+          <Route component={NotFound}/>
+        </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/CadastrarLivros.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
+
+const CadastrarLivros = (props) => {
+    const [livro, setLivro] = useState({
+        id: props.livro.id,
+        isbn: props.livro.isbn,
+        titulo: props.livro.titulo,
+        autor: props.livro.autor,
+    })
+
+    const [redirecionar, setRedirecionar] = useState(false);
+
+    const handleLivroForm = e => {
+        e.preventDefault();
+        if(props.editarLivro){
+            props.editarLivro(livro);
+        } else{
+            props.inserirLivro(livro);
+        }
+        setRedirecionar(true);
+    }
+
+    if(redirecionar){
+        return <Redirect to='/'/>
+    }
+
+    return(
+        <form onSubmit={handleLivroForm}>
+            {props.editarLivro ?  <h1>Editar livro</h1> : <h1>Cadastrar livro</h1>}
+            <p>
+                <label>
+                    ISBN: Formato - (
+                        <span style={{color: 'red'}}> 978-85-7522-xxx-x </span>
+                    )
+                </label>
+                <input 
+                    type='text' 
+                    autoFocus 
+                    // defaultValue={livro.isbn}
+                    id="fisbn" 
+                    pattern='^978-85-7522-[0-9]{3}-[0-9]{1}$' 
+                    value={livro.isbn}
+                    onChange={e => {
+                        setLivro({
+                            ...livro,
+                            isbn: e.target.value
+                        })
+                    }}
+                />
+            </p>
+            <p>
+                <label>Título</label>
+                <input 
+                    type='text'
+                    // defaultValue={livro.titulo}
+                    // ref='titulo' 
+                    id='ftitulo' 
+                    required 
+                    value={livro.titulo}
+                    onChange={e => {
+                        setLivro({
+                            ...livro,
+                            titulo: e.target.value
+                        })
+                    }}
+                />
+            </p>
+            <p>
+                <label>Autor</label>
+                <input 
+                    type='text' 
+                    // defaultValue={livro.autor}
+                    // ref='autor'
+                    id='fautor' 
+                    required 
+                    value={livro.autor}
+                    onChange={e => {
+                        setLivro({
+                            ...livro,
+                            autor: e.target.value
+                        })
+                    }}
+                />
+            </p>
+            <p>
+                <button type='submit' className='botao cadastrar'>{props.editarLivro ? 'Salvar' : 'Cadastrar'}</button>
+            </p>
+        </form>
+    );
+}
+
+export default CadastrarLivros;
